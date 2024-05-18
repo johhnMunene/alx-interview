3b5281d420207609aee3f8c8b3ae21012eaed740
johhnMunene <Edwardsjohnny64@gmail.com>
2024-05-18 07:57:58 -0500

    updates

--- 0x05-nqueens/0-nqueens.py
+++ 0x05-nqueens/0-nqueens.py
@@ -1,55 +1,79 @@
-#!/usr/bin/python3
-"""
-N queens
-"""
-
+#!/usr/bin/env python3
 import sys
 
-if len(sys.argv) != 2:
-    print('Usage: nqueens N')
-    exit(1)
+def init_board(n):
+    """
+    Initializes an empty chessboard of size n x n.
+    """
+    board = [['' for _ in range(n)] for _ in range(n)]
+    return board
 
-try:
-    n_q = int(sys.argv[1])
-except ValueError:
-    print('N must ba a number')
-    exit(1)
+def get_solution(board):
+    """
+    Returns the solution from the given board configuration.
+    """
+    solution = []
+    for r in range(len(board)):
+        for c in range(len(board[r])):
+            if board[r][c] == "Q":
+                solution.append([r, c])
+                break
+    return solution
 
-if n_q < 4:
-    print('N must ba at least 4')
-    exit(1)
-
-
-def solve_nqueens(n):
-    ''' self descriptive '''
-    if n == 0:
-        return [[]]
-    inner_solution = solve_nqueens(n - 1)
-    return [solution + [(n, i + 1)]
-            for i in range(n_q)
-            for solution in inner_solution
-            if safe_queen((n, i + 1), solution)]
-
-
-def attack_queen(square, queen):
-    '''self descriptive'''
-    (row1, col1) = square
-    (row2, col2) = queen
-    return (row1 == row2) or (col1 == col2) or\
-        abs(row1 - row2) == abs(col1 - col2)
-
-
-def safe_queen(sqr, queens):
-    '''self descriptive'''
-    for queen in queens:
-        if attack_queen(sqr, queen):
+def is_safe(board, row, col):
+    """
+    Checks if it's safe to place a queen at board[row][col].
+    """
+    n = len(board)
+    # Check column
+    for i in range(row):
+        if board[i][col] == 'Q':
             return False
+    # Check upper left diagonal
+    i, j = row, col
+    while i >= 0 and j >= 0:
+        if board[i][j] == 'Q':
+            return False
+        i -= 1
+        j -= 1
+    # Check upper right diagonal
+    i, j = row, col
+    while i >= 0 and j < n:
+        if board[i][j] == 'Q':
+            return False
+        i -= 1
+        j += 1
     return True
 
-
-for answer in reversed(solve_nqueens(n_q)):
-    result = []
-    for p in [list(p) for p in answer]:
-        result.append([i - 1 for i in p])
-    print(result)
+def solve_nqueens(board, row, solutions):
+    """
+    Recursive function to solve the N-queens puzzle.
+    """
+    n = len(board)
+    if row == n:
+        solutions.append(get_solution(board))
+        return
     
+    for col in range(n):
+        if is_safe(board, row, col):
+            board[row][col] = "Q"
+            solve_nqueens(board, row + 1, solutions)
+            board[row][col] = ""  # Backtrack
+
+if __name__ == "__main__":
+    if len(sys.argv) != 2:
+        print("Usage: nqueens N")
+        sys.exit(1)
+    if not sys.argv[1].isdigit():
+        print("N must be a number")
+        sys.exit(1)
+    n = int(sys.argv[1])
+    if n < 4:
+        print("N must be at least 4")
+        sys.exit(1)
+    
+    board = init_board(n)
+    solutions = []
+    solve_nqueens(board, 0, solutions)
+    for sol in solutions:
+        print(sol)
