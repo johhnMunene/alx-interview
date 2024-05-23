4adc2fa5a9c9910b82b793a8f7b0e608f41fa5d8
johhnMunene <Edwardsjohnny64@gmail.com>
2024-05-23 16:04:02 -0500

    star wars APi

--- 0x06-starwars_api/0-starwars_characters.js
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -1,23 +1,25 @@
 #!/usr/bin/node
+// A script that prints all characters of a Star Wars movie
 
 const request = require('request');
 
-const movieId = process.argv[2];
-
-const url = `https://swapi-api.hbtn.io/api/films/${movieId}`;
-
-request(url, async (err, res, body) => {
-  err && console.log(err);
-
-  const charactersArray = (JSON.parse(res.body).characters);
-  for (const character of charactersArray) {
-    await new Promise((resolve, reject) => {
-      request(character, (err, res, body) => {
-        err && console.log(err);
-
-        console.log(JSON.parse(body).name);
-        resolve();
-      });
-    });
+const movie = process.argv[2];
+const api = 'https://swapi-api.hbtn.io/api/';
+const url = api + 'films/' + movie + '/';
+request.get({ url: url }, function (error, response, body) {
+  if (!error) {
+    const characters = JSON.parse(body).characters;
+    order(characters);
   }
 });
+
+function order (characters) {
+  if (characters.length > 0) {
+    request.get({ url: characters.shift() }, function (err, res, body) {
+      if (!err) {
+        console.log(JSON.parse(body).name);
+        order(characters);
+      }
+    });
+  }
+}
