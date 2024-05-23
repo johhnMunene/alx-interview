f5724196d491b5904ed0b4ff2696c50e384be5c6
johhnMunene <Edwardsjohnny64@gmail.com>
2024-05-23 16:08:51 -0500

    star wars APi

--- 0x06-starwars_api/0-starwars_characters.js
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -2,28 +2,36 @@
 const request = require('request');
 const API_URL = `https://swapi-api.hbtn.io/api/films/${process.argv[2]}`;
 
-// a function to return a list of promises for each character fetched from the api
-function getCharacterNames (characters) {
-  const names = characters.map((character) => {
+// Function to return a list of promises for each character fetched from the API
+function getCharacterNames(characters) {
+  return characters.map(character => {
     return new Promise((resolve, reject) => {
       request(character, (error, _, body) => {
-        if (error) reject(Error('something went wrong'));
-        else resolve(JSON.parse(body).name);
+        if (error) {
+          reject(`Error fetching character: ${error}`);
+        } else {
+          resolve(JSON.parse(body).name);
+        }
       });
     });
   });
-  return names;
 }
 
 request(API_URL, (error, _, body) => {
-  if (error) console.log(`error: ${error}`);
-  else {
+  if (error) {
+    console.error(`Error fetching film details: ${error}`);
+  } else {
     const targetFilm = JSON.parse(body);
     const namesPromises = getCharacterNames(targetFilm.characters);
-    Promise.all(namesPromises).then((result) => {
-      result.forEach((element) => {
-        console.log(element);
+    Promise.all(namesPromises)
+      .then(names => {
+        names.forEach(name => {
+          console.log(name);
+        });
+      })
+      .catch(error => {
+        console.error(error);
       });
-    });
   }
 });
+
